e8d02755b99db3a9c5591639a8646c8ea8228257
413XPK <alexparktoronto@gmail.com>
2021-10-28 18:49:58 -0400

    update urls and views
    made by alex

--- main_app/urls.py
+++ main_app/urls.py
@@ -1,11 +1,19 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 from . import views
 
 urlpatterns = [
-    # alex made a comment
+
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),
     
+    path('marketplace/', views.marketplace, name='index'),
+    path('marketplace/<int:product_id>', views.products_detail, name='detail'),
+    path('marketplace/create>', views.ProductCreate.as_view(), name='product_create'),
+    path('marketplace/update>', views.ProductUpdate.as_view(), name='product_update'),
+    path('marketplace/delete>', views.ProductDelete.as_view(), name='product_delete'),
+    path('marketplace/<int:product_id/add_photo', views.add_photo, name='add_photo'),
+    path('accounts/', include('django.contrib.auth.urls')),
+    path('accounts/signup', views.signup, name='signup'),
 ]
