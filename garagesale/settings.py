ed08e219ebd3e001d19ad2e022056b1932c40a11
413XPK <alexparktoronto@gmail.com>
2021-10-27 22:18:54 -0400

    alex commit

--- garagesale/settings.py
+++ garagesale/settings.py
@@ -32,7 +32,6 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'main_app',
-    'djmoney',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- main_app/urls.py
+++ main_app/urls.py
@@ -1,8 +1,10 @@
+from django.contrib import admin
 from django.urls import path
 
 from . import views
 
 urlpatterns = [
+    path('admin/', admin.site.urls),
     path('', views.home, name='home'),
     ## Mehdi Making a Change
 ]
