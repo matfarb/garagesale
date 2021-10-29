724da14d15941f23a3295f3d34d68242348f4d9c
Mehdi Salemi <medxman@Mehdis-MacBook-Pro.local>
2021-10-28 22:37:50 -0400

    product create - needs template

--- main_app/urls.py
+++ main_app/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
+from . import views
 
 from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
     path('inventory/', views.inventory, name='inventory'),
+    path('inventory/create', views.ProductCreate.as_view(), )
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import login
 from django.contrib.auth.decorators import login_required
 from .models import Product
+from django.views.generic.edit import CreateView
 
 def home(request):
   return render(request, 'home.html')
@@ -9,3 +10,8 @@ def home(request):
 def inventory(request):
   products = Product.objects.filter(user=request.user)
   return render(request, 'products/inventory.html', { 'products': products})
+
+class ProductCreate(CreateView):
+  model = Product
+  fields = '__all__'
+
