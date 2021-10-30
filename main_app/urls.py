b2235ccf34e41103125d05ddbc3cb232e2784ae1
Mehdi Salemi <medxman@Mehdis-MacBook-Pro.local>
2021-10-30 13:10:18 -0400

    this sould be working

--- main_app/urls.py
+++ main_app/urls.py
@@ -6,5 +6,5 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('inventory/', views.inventory, name='inventory'),
-    path('inventory/create', views.ProductCreate.as_view(), )
+    path('inventory/create', views.ProductCreate.as_view(), name='product_create')
 ]
