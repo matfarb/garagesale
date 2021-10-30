5d2c683405f87325eab81e517e1881b688d0aa85
413XPK <alexparktoronto@gmail.com>
2021-10-30 16:51:09 -0400

    add alexs code

--- main_app/urls.py
+++ main_app/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
 
 from . import views
@@ -6,5 +6,14 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('inventory/', views.inventory, name='inventory'),
-    path('inventory/create', views.ProductCreate.as_view(), name='product_create')
+    path('inventory/create', views.ProductCreate.as_view(), name='product_create'),
+
+
+    path('marketplace/', views.marketplace, name='index'),
+    path('marketplace/<int:product_id>', views.products_detail, name='detail'),
+    path('marketplace/update', views.ProductUpdate.as_view(), name='product_update'),
+    path('marketplace/delete', views.ProductDelete.as_view(), name='product_delete'),
+    path('marketplace/<int:product_id/add_photo', views.add_photo, name='add_photo'),
+    path('accounts/', include('django.contrib.auth.urls')),
+    path('accounts/signup', views.signup, name='signup'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import login
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import UserCreationForm
+
 from .models import Product
 from django.views.generic.edit import CreateView
 
@@ -20,3 +22,22 @@ class ProductCreate(CreateView):
       return super().form_valid(form)
 
   success_url = '/inventory/'
+
+def signup(request):
+  error_message = ''
+  if request.method == 'POST':
+    # This is how to create a 'user' form object
+    # that includes the data from the browser
+    form = UserCreationForm(request.POST)
+    if form.is_valid():
+      # This will add the user to the database
+      user = form.save()
+      # This is how we log a user in via code
+      login(request, user)
+      return redirect('index')
+    else:
+      error_message = 'Invalid credentials - try again'
+  # A bad POST or a GET request, so render signup.html with an empty form
+  form = UserCreationForm()
+  context = {'form': form, 'error_message': error_message}
+  return render(request, 'registration/signup.html', context)
