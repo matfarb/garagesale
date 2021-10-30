f7157ab9b1437ab253a76a2f6e6cc3cd9bac3171
matfarb <mathew30@live.ca>
2021-10-30 18:06:12 -0400

    Working on templates

--- main_app/views.py
+++ main_app/views.py
@@ -10,9 +10,19 @@ from django.views.generic.edit import CreateView
 def home(request):
   return render(request, 'home.html')
 
+def marketplace(request):
+  products = Product.objects.all()
+  return render(request, 'products/marketplace.html', { 'products': products})
+
 def inventory(request):
   products = Product.objects.filter(user=request.user)
   return render(request, 'products/inventory.html', { 'products': products})
+
+def products_detail(request, product_id):
+  product = Product.objects.get(id=product_id)
+  return render(request, 'products/detail.html', { 
+    'product': product
+  })
 
 class ProductCreate(CreateView):
   model = Product
