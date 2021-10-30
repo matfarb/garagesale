7187dceadc6a237a7ae7883f31a4008ba49fbcdd
matfarb <mathew30@live.ca>
2021-10-30 16:58:59 -0400

    added user signup and base.html

--- garagesale/settings.py
+++ garagesale/settings.py
@@ -10,10 +10,10 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
-from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 # Quick-start development settings - unsuitable for production
@@ -121,6 +121,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [ os.path.join(BASE_DIR,'static') ]
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'assets')
 
 LOGIN_REDIRECT_URL = '/'
 
--- main_app/urls.py
+++ main_app/urls.py
@@ -6,5 +6,6 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('inventory/', views.inventory, name='inventory'),
-    path('inventory/create', views.ProductCreate.as_view(), name='product_create')
+    path('inventory/create', views.ProductCreate.as_view(), name='product_create'),
+    path('accounts/signup/', views.signup, name='signup'),
 ]
