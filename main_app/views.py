64525b195e8ef9df9f807299549fe7859eadb95f
Mehdi Salemi <medxman@Mehdis-MacBook-Pro.local>
2021-10-30 17:16:35 -0400

    noo

--- main_app/views.py
+++ main_app/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import UserCreationForm
+
 from .models import Product
 from django.views.generic.edit import CreateView
 
@@ -20,3 +23,17 @@ class ProductCreate(CreateView):
       return super().form_valid(form)
 
   success_url = '/inventory/'
+
+def signup(request):
+  error_message = ''
+  if request.method == 'POST':
+    form = UserCreationForm(request.POST)
+    if form.is_valid():
+      user = form.save()
+      login(request, user)
+      return redirect('inventory')
+    else:
+      error_message = 'Invalid sign up - try again'
+      form = UserCreationForm()
+      context = {'form': form, 'error_message': error_message}
+      return render(request, 'registration/signup.html', context)
