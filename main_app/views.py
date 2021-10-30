b8c587e8c29154f19004c403f73604cf323296bf
matfarb <mathew30@live.ca>
2021-10-30 17:12:57 -0400

    commented out future implementations

--- main_app/views.py
+++ main_app/views.py
@@ -27,7 +27,6 @@ class ProductCreate(CreateView):
 def signup(request):
   error_message = ''
   if request.method == 'POST':
-<<<<<<< HEAD
     form = UserCreationForm(request.POST)
     if form.is_valid():
       user = form.save()
@@ -35,20 +34,6 @@ def signup(request):
       return redirect('inventory')
     else:
       error_message = 'Invalid sign up - try again'
-=======
-    # This is how to create a 'user' form object
-    # that includes the data from the browser
-    form = UserCreationForm(request.POST)
-    if form.is_valid():
-      # This will add the user to the database
-      user = form.save()
-      # This is how we log a user in via code
-      login(request, user)
-      return redirect('index')
-    else:
-      error_message = 'Invalid credentials - try again'
-  # A bad POST or a GET request, so render signup.html with an empty form
->>>>>>> 5d2c683405f87325eab81e517e1881b688d0aa85
   form = UserCreationForm()
   context = {'form': form, 'error_message': error_message}
   return render(request, 'registration/signup.html', context)
