12f0cc741433df6a8470f4cfce49e162f93cee00
Mehdi Salemi <medxman@Mehdis-MacBook-Pro.local>
2021-11-01 20:07:29 -0400

    message

--- main_app/models.py
+++ main_app/models.py
@@ -1,26 +1,12 @@
 from django.db import models
+from django.shortcuts import redirect
 from django.urls import reverse
 from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
-# https://simpleisbetterthancomplex.com/tutorial/2016/07/22/how-to-extend-django-user-model.html
-class Profile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    favorite_color = models.CharField(max_length=50)
-    io = models.TextField(max_length=500, blank=True)
-    location = models.CharField(max_length=30, blank=True)
-    birth_date = models.DateField(null=True, blank=True)
-
-@receiver(post_save, sender=User)
-def create_user_profile(sender, instance, created, **kwargs):
-    if created:
-        Profile.objects.create(user=instance)
-
-@receiver(post_save, sender=User)
-def save_user_profile(sender, instance, **kwargs):
-    instance.profile.save()
-
+from multiselectfield import MultiSelectField
+# pip install django-multiselectfield
 
 class Product(models.Model):
     title = models.CharField(max_length=50)
@@ -43,3 +29,18 @@ class ProductPhoto(models.Model):
 
   def __str__(self):
     return f"Photo for product_id: {self.product_id} @{self.url}"
+
+class Profile(models.Model):
+    COLOR_CHOICES = [
+        ("blue", "Blue"),
+    ]
+
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    bio = models.TextField(max_length=500, blank=True)
+    favorite_color = models.CharField(max_length=50, blank=True, default="blue")
+
+    def __str__(self):
+        return str(self.user)
+    
+    def get_absolute_url(self):
+        return reverse('profile')
